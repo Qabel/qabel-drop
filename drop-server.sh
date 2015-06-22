614572fb82cf737fc777beb12c32403bb10238a9
hartie95 <hartie95@hartie95>
2015-06-22 12:23:56 +0200

    Added shell script for server starting and stopping (normaly uses start stop deamon, but also has alternativ using lsof)

---
+++ drop-server.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+pythonpath=$(command -v python2 || command -v python)
+startstopdeamon="/sbin/start-stop-daemon"
+
+start()
+{
+	if [ -f "$startstopdeamon" ]; then
+		echo "Starting Server ..."
+		/sbin/start-stop-daemon --start --background --pidfile drop.pid -m -d ./ --exec $pythonpath -- 'drop_server.py'
+	else
+		pythonpath qabel-drop/drop_server.py &
+	fi
+}
+
+stop()
+{
+	if [ -f "$startstopdeamon" ]; then
+		echo "Stopping Server ..."
+		/sbin/start-stop-daemon --stop --pidfile drop.pid --remove-pidfile
+	else
+		lsof -P | grep ':6000' | awk '{print $2}' | xargs kill  
+	fi
+}
+
+help()
+{
+	echo "usage:"
+	echo "start  -  starts the drop-server"
+	echo "stop   -  stops the drop-server"
+	echo "python -  get python path"
+}
+
+
+
+case "$1" in
+	"start") start	
+		;;
+	"stop") stop
+		;;
+	"python") echo $pythonpath
+		;;
+	*) help
+		;;
+esac
+
+
