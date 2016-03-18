976184ac88a7fffe75889760af178d996e8cc99e
Jens Kadenbach <gitdax@daxbau.net>
2016-03-18 16:25:11 +0100

    Start prometheus threads in background

--- drop_server/app.py
+++ drop_server/app.py
@@ -1,8 +1,31 @@
+from http.server import HTTPServer
+import logging
+
 from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
+from drop_server.backend.monitoring import PrometheusEndpointServer
+import prometheus_client
+
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 with app.app_context():
     db.init_app(current_app)
+
+if app.config['PROMETHEUS_ENABLE']:
+    prom_adr = app.config['PROMETHEUS_ADDRESS']
+    for port in app.config['PROMETHEUS_PORTS']:
+        try:
+            httpd = HTTPServer((prom_adr, port), prometheus_client.MetricsHandler)
+        except OSError as e:
+            print(e)
+            continue  # Try next port
+        thread = PrometheusEndpointServer(httpd)
+        thread.daemon = True
+        thread.start()
+        logger.info('Serving prometheus on {}:{}'.format(prom_adr, port))
+        break
+    else:
+        raise OSError('No port for prometheus metrics available')
--- drop_server/backend/monitoring.py
+++ drop_server/backend/monitoring.py
@@ -1,4 +1,5 @@
 from prometheus_client import Summary, Counter
+import threading
 
 REQUEST_TIME = Summary('drop_request_processing',
                        'Time spent processing request', ['status', 'method'])
@@ -7,7 +8,16 @@ DROP_SAVE_ERROR = Counter('drop_save_message_failed',
                           'Exception when trying to save a drop message')
 
 DROP_SENT = Counter('drop_messages_delivered',
-                          'Amount of delivered drop messages')
+                    'Amount of delivered drop messages')
 
 DROP_RECEIVED = Counter('drop_messages_received',
-                    'Amount of received drop messages')
+                        'Amount of received drop messages')
+
+
+class PrometheusEndpointServer(threading.Thread):
+    def __init__(self, httpd, *args, **kwargs):
+        self.httpd = httpd
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        self.httpd.serve_forever()
