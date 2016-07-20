7c91c4e757f6010862bde12b403a128d01c428d5
Marian Beermann <beermann@qabel.de>
2016-07-20 10:55:46 +0200

    Remove unusable FCM_MESSAGES_PROCESSED

--- drop_service/monitoring.py
+++ drop_service/monitoring.py
@@ -9,7 +9,3 @@ DROP_RECEIVED = Counter('drop_messages_received',
 
 FCM_CALLS = Counter('notify_fcm_calls',
                     'Number of notify_topic_subscribers FCM API calls', ['exception'])
-
-# We only receive these stats for downstream messages - we intend to use topic messages
-#FCM_MESSAGES_PROCESSED = Counter('notify_fcm_messages',
-#                                 'Number of messages processed by FCM', ['outcome'])
