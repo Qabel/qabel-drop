f33c415545dc850cfcbf028f8c8f143f6df4274e
Marian Beermann <beermann@qabel.de>
2016-07-20 13:03:45 +0200

    test_fcm_doesnt_explode: this test is pointless now
    If it where to explode it would blow up the worker thread now, not the
    test thread. Different thread, different stack, different SEH.

--- tests/test_notify.py
+++ tests/test_notify.py
@@ -21,13 +21,3 @@ def test_fcm_basic(drop_messages):
         'drop-id': 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopo'
     }
     api.notify_topic_subscribers.assert_called_once_with(topic_name=drop1.drop_id, data_message=data_message)
-
-
-def test_fcm_doesnt_explode(drop_messages):
-    drop1, drop2 = drop_messages
-    api = MagicMock(spec=FCMNotification)()
-    api.notify_topic_subscribers.side_effect = AuthenticationError('Well that didn\'t work!')
-
-    with ThreadPoolExecutor() as executor:
-        fcm = FCM(api, executor)
-        fcm.notify(drop1)
